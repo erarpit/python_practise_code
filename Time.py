82ecc9fae27427c457b8b6ccd189050bf7d0de69
Arpit <33867976+erarpit@users.noreply.github.com>
2017-12-05 20:41:58 +0530

    Add files via upload

---
+++ Time.py
@@ -0,0 +1,17 @@
+print('This program will wish you Goodmornig! ...According to time You will enter\n')
+
+time = int(input('Enter what is the time..? -\n '))
+if time<9:
+    print('Good morning Sir...!!')
+elif time<12:
+    print('good afternoon Sir...!!')
+elif time<18:
+    print('good evening sir...!!!')
+elif time<22:
+    print('Good Day....!!!')
+else:
+    print('Good night')
+print('Thank you\n')
+
+
+        
